0d56c885d40792d04a2b7963a5a585bea5d88f68
nikushatavartkiladze13 <nikatavartkiladze09@icloud.com>
2024-09-12 20:52:05 +0400

    second commit

---
+++ day_24/classwork/classwork.py
@@ -0,0 +1,10 @@
+student = ["envera", "laerti",  "meraba", "jemala"]
+print(student)
+
+odd = [2,4,6,8,10,12,14,16,18,20]
+print(odd)
+
+list = [5,10,15,20,30]
+print(list)
+#len - სიგრძე
+print(len(list))
