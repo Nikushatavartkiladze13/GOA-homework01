7cf27050fb99529cf882bab4861d5317d1dd36ae
nikushatavartkiladze13 <nikatavartkiladze09@icloud.com>
2024-09-30 15:13:40 +0400

    second commit

---
+++ day_28/classwork/classwork.py
@@ -0,0 +1,10 @@
+#nubmer_lists = [1,2,3,4,5,6,7,8,9,10]
+#print(nubmer_lists[1])
+#print(nubmer_lists[3])
+#print(nubmer_lists[5])
+#print(nubmer_lists[7])
+#print(nubmer_lists[9])
+#car_lists = ["bmw","mercedes","opel","kia","ford","niva","volvo","fiat","nissan","toyota","subaru"]
+#for i in range(len(car_lists)):
+#    print(car_lists[i])
+
