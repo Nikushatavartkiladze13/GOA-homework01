77ecdcb5d63b5d51b3d0bbab02e01555634446b3
nikushatavartkiladze13 <nikatavartkiladze09@icloud.com>
2024-11-30 15:32:56 +0400

    second commit

--- day_18/classwork/classwork.py
+++ day_18/classwork/classwork.py
@@ -1,17 +1,4 @@
-#name = "nika"
-#if name!="nika":
-#    print("chad")
-#else:
-#    print("virgine") 
 
-#product_name = input("enter product name")
-
-#if product_name == "ლავაში" :
-#    print("ვიყიდოთ ლავაში")
-#elif product_name == "დაჭრილი პური":
-#    print("ვიყიდოთ დაჭრილი პური")
-#else: 
-#    print("არვიყიდოთ არაფერი")
  
 # მომხმარებელს შემოატანინოთ რიცხვი, შემდეგ დაბეჭდეთ ეს რიცხვი კენტია თუ ლუწი.
 num = int(input("enter your number"))
--- day_20/homework/hw01.py
+++ day_20/homework/hw01.py
@@ -1,5 +1,9 @@
 for i in range(0,100,2):
     print((str(i)) + "is even")
 
+
+
+
+
 for i in range(1, 100, 2):
     print(str(i) +"is odd")
